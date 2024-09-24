449c2aa1fc7a403324e88e1789fed53ef183fdce
RedHellier <redhellier.gmail.com>
2024-09-24 11:02:58 +0100

    Header and NavBar added, Style and Size Testing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 
 var entries = ["1999","2005","2011","2024"];
 
+const list = document.createElement('ul');
+document.body.appendChild(list);
+document.body.insertBefore
 
 for (let en of entries) {
-
+    var listItem = document.createElement('li');
+    listItem.innerHTML = en;
+    list.appendChild(listItem)
 }
