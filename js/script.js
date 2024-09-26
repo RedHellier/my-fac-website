e9e818b4527fc2bdf5b3401762916b5076e57c5b
RedHellier <redhellier.gmail.com>
2024-09-26 13:39:07 +0100

    Changed script.js, can now swap between active tabs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+var tabs = document.getElementsByClassName("tab")
+console.log(tabs)
 
-var entries = ["1999","2005","2011","2024"];
+for (let t of tabs) {
+    t.onclick = function() {setActiveTab(t)};
+}
+/*
+var aboutTab = document.getElementById("about");
+tabs.push(aboutTab)
+var workTab = document.getElementById("work");
+tabs.push(workTab)
+var favesTab = document.getElementById("faves");
+tabs.push(favesTab)*/
 
-const list = document.createElement('ul');
-document.body.appendChild(list);
-document.body.insertBefore
-
-for (let en of entries) {
-    var listItem = document.createElement('li');
-    listItem.innerHTML = en;
-    list.appendChild(listItem)
+function setActiveTab(element) {
+    if (element.getAttribute("active")!=="true") {
+        for (let t of tabs) {
+            if (element.id===t.id) {
+                t.setAttribute("active","true");
+            } else {
+                t.setAttribute("active","false");
+            }
+        }
+    }
 }
