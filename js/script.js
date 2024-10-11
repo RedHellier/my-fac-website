9c495d7f0ad574a8cf935a431d87d6e49684e9d8
RedHellier <redhellier.gmail.com>
2024-10-11 14:11:53 +0100

    Removed about.html, Nav buttons that set content pages to either active or not

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 var tabs = document.getElementsByClassName("tab-page")
-console.log(tabs)
+var contents = document.getElementsByClassName("content")
 
 for (let t of tabs) {
-    t.onclick = function() {setActiveTab(t)};
+    t.onclick = function() {
+        setActiveTab(t)
+    };
+}
+
+for (let c of contents) {
+    console.log(c)
 }
 
 function setActiveTab(element) {
-    if (element.getAttribute("active")!=="true") {
+    if (element.getAttribute("active-tab")!=="true") {
+
         for (let t of tabs) {
             if (element.id===t.id) {
-                t.setAttribute("active","true");
+                t.setAttribute("active-tab","true");
             } else {
-                t.setAttribute("active","false");
+                t.setAttribute("active-tab","false");
+            }
+        }
+        
+        for (let c of contents) {
+            if (element.id===c.id) {
+                c.setAttribute("active-page","true");
+            } else {
+                c.setAttribute("active-page","false");
             }
         }
     }
