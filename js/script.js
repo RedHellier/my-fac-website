b53c7192985acb35b70ef52dfa3c1f68c1598c81
RedHellier <redhellier@gmail.com>
2024-10-16 17:58:45 +0100

    --Moving work stations, trying out figure dynamic movement

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const tabs = document.getElementsByClassName("tab-page")
 const contents = document.getElementsByClassName("content")
+let figures = document.getElementsByClassName("figure")
+
+/*for (let i=0; i< figures.length; i++) {
+    let top = i*85;;
+    figures.item(i).style.top = top + "vh";
+}*/
 
 for (let t of tabs) {
     t.onclick = function() {
         setActiveTab(t)
     };
 }
-
+/**
+ * Returns the sum of all numbers passed to the function.
+ * @param {HTMLElement | number} element A positive or negative number
+ */
 function setActiveTab(element) {
     if (element.getAttribute("active-tab")!=="true") {
 
@@ -37,8 +46,15 @@ function scrollFunction() {
     let scrollDistance = document.getElementById("scrollbar").offsetHeight - document.getElementById("scroller").offsetHeight;
     document.getElementById("scroller").style.marginTop = multiplier*scrollDistance + "px";
     if (multiplier > 0.99) {
-        document.getElementById("footer").style.bottom = "0";
+        document.getElementById("header").style.top = "-100px";
     } else {
-        document.getElementById("footer").style.bottom = "-100px";
+        document.getElementById("header").style.top = "0";
     }
+    figureScroll(multiplier);
+}
+
+function figureScroll(pageDistance) {
+    let transitionPoint = 1/figures.length;
+    let currentSection = Math.floor(pageDistance/transitionPoint);
+    console.log(currentSection)
 }
