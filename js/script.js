f358d1ea4930a2ff9e691ca3c0f0abee05c1fd34
RedHellier <redhellier.gmail.com>
2024-10-16 23:55:37 +0100

    Scrollbar now displays figure titles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const tabs = document.getElementsByClassName("tab-page")
 const contents = document.getElementsByClassName("content")
 let figures = document.getElementsByClassName("figure")
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+let currentFigure = 0;
 
-/*for (let i=0; i< figures.length; i++) {
-    let top = i*85;;
-    figures.item(i).style.top = top + "vh";
-}*/
 
 for (let t of tabs) {
     t.onclick = function() {
         setActiveTab(t)
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        currentFigure = 0;
     };
 }
 /**
@@ -44,17 +46,15 @@ window.onscroll = function() {
 function scrollFunction() {
     let multiplier = window.scrollY/(document.documentElement.scrollHeight-window.innerHeight);
     let scrollDistance = document.getElementById("scrollbar").offsetHeight - document.getElementById("scroller").offsetHeight;
+    currentFigure = Math.floor(multiplier/(1.01/figures.length));
+
     document.getElementById("scroller").style.marginTop = multiplier*scrollDistance + "px";
+
     if (multiplier > 0.99) {
         document.getElementById("header").style.top = "-100px";
     } else {
         document.getElementById("header").style.top = "0";
     }
-    figureScroll(multiplier);
-}
 
-function figureScroll(pageDistance) {
-    let transitionPoint = 1/figures.length;
-    let currentSection = Math.floor(pageDistance/transitionPoint);
-    console.log(currentSection)
+    document.getElementById("scrollerText").innerHTML = figures.item(currentFigure).getAttribute("title");
 }
