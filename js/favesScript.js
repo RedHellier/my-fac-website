22f2860e3d5071c2fe467e14fed698cc703d7ab1
RedHellier <redhellier.gmail.com>
2024-12-01 20:53:37 +0000

    Responsiveness for two breakpoints partially implemented

---
+++ js/favesScript.js
@@ -0,0 +1,83 @@
+const faves = {
+    movie : {
+        index: 0,
+        images: ["Marcel.jpg", "memento.jpg", "petmam.jpg"],
+        titles: ["Marcel the Shell with Shoes On", "Memento", "Petite Maman"],
+        details: [
+            [
+                "<strong>2021 | Dean Fleischer Camp</strong>",
+                "<strong>Synopsis > </strong>A film bout a shell with shoes on looking for his family",
+                "<strong>My Review > </strong>There is no other cinematic medium that is as careful as stop-motion animation and Marcel truly embodies that care. The filmakers had to shoot every scene of this movie twice, each time perfectly replicating camera position, lighting and speed. And they pull it off perfectly, every animated critter feels as tangible and real as the world they inhabit. This feeling of realness is also achieved thanks to the amazing dialogue between Marcel, Connie and Dean. Marcel's worldview is revealed through the way he answers Dean's questions, with a lack of prior context or baggage that we all accept as normal. He removes the narrative that we all just believe without question and in doing so shows us that we can just exist and work through through the challenges that we have to deal with every day and that can be enough."
+            ],
+            [
+                "<strong>2000 | Christopher Nolan</strong>",
+                "<strong>Synopsis > </strong>A man with short term memory loss tries to find his wife's murderer",
+                "<strong>My Review > </strong>We all know that Christopher Nolan excels at writing over complicated time jumping movies, but Memento is the film where he pairs that well with excellently written characters. We are dropped into a story at the peak of it's action and the end of it's narrative. We judge the characters based off what we know but then have to rethink our judgements as we delve back into the past. We place our trust in certain characters who end up betraying that trust, an experience shared by our protaganist Leonard. Throughout most of the runtime he has no control and believes he must do what the nearest person to him says is the right course of action. This makes his decision at the end of the movie to give himself agency so compelling."
+            ],
+            [
+                "<strong>2021 | Celine Sciamma</strong>",
+                "<strong>Synopsis > </strong>While visiting her grandmother's house, this little girl makes a friend that seems very familiar",
+                "<strong>My Review > </strong>We want to protect our children from the pain we experience as adults, but often the attempt to protect them just causes them the pain we wanted to avoid. Petite Maman is a film where our main character is allowed to share in that pain and grow closer to her mother because of it. It explores it through many childlike activities, like exploring the woods, acting out stories or making a mess in the kitchen. Nelly may not undertand initially but by being open and honest with Marion she comes to terms with it and can comfort her mother through her pain."
+            ]
+        ]
+    },
+    game: {
+        index: 0,
+        images: ["hknight.jpg", "skyrim.jpg", "botw.jpg"],
+        titles: ["Hollow Knight", "Skyrim", "The Legend of Zelda: Breath of the Wild"],
+        details: [
+            [
+                "<strong>2017 | Team Cherry</strong>",
+                "<strong>Synopsis > </strong>Delve into the world of hallow nest to save it from a plague that has infected its many inhabitants",
+                "<strong>My Review > </strong>Metroidvanias are hot right now, but none are as well paced or exciting as hollow knight. The task at hand seems impossible, you are small and irrelevant, or so it seems at first. As you go on, defeating boss after boss and exploring each area, you can find all sorts of small glimpses into the lore of this place and of your character. Who made you? Why are you the only one who can save the world? What is the grubmother? No really, what is she? You may not discover the answers to all of your questions during a playthrough but that idea that there is something larger going on here is inescapable and exciting."
+            ],
+            [
+                "<strong>2011 | Bethesda</strong>",
+                "<strong>Synopsis > </strong>An epic adventure of self discovery and dragon slaying",
+                "<strong>My Review > </strong>It's iconic. You awake on the way to your execution, not knowing who you are or why you're there, and are fed breadcrumbs of story to pull you into an expansive narrative. Then once you're in there are hooks everywhere that keep pulling you in every direction. Do you want to be an expert thief, a powerful wizard or the saviour of a rebel uprising. Each narrative arc is expertly crafted to make you feel like the most important person in the world. And then there are the smaller narratives dotted around in abandoned shacks, caves, and ruins. Each one gives you a small insight into this larger complex world, showing you how individuals fare in this world controlled by such large factions. Each story, large or small, is important and well crafted by the writers. You just never get bored."
+            ],
+            [
+                "<strong>2017 | Nintendo</strong>",
+                "<strong>Synopsis > </strong>After 100 years asleep, you must once again save hyrule from the many monsters that threaten it",
+                "<strong>My Review > </strong>When this came out I didn't have a switch so like many people I bought one spicifically to play this game. And I was not dissapointed. Never have I felt freer to explore the world and the story at my own pace and in my own way. Thanks to innovative new mechanics, combat and exploration can be tackled how you see fit. Whether that is just by swinging your sword, throwing bombs or by grabbing massive logs to drop on enemies heads, you are in control. And with exciting things to explore all around the map you cannot help but be diverted from your path by something that looks fun or interesting. Very intentionally however, even if you stray from the path, the designers have made it so that you always find your path again by placing important locations in specific places around the map. Shrines to gain hearts, villages to start quests, or puzzles to solve are always only a quick detour away."
+            ]
+        ]
+    },
+    album: {
+        index: 0,
+        images: ["abbeyroad.webp", "ys.webp", "abso.jpg"],
+        titles: ["Abbey Road", "Ys", "Absolutely"],
+        details: [
+            [
+                "<strong>1969 | The Beatles</strong>",
+                "<strong>Top Tracks > </strong>Golden Slumbers, Carry That Weight, The End",
+                "<strong>My Review > </strong>Of the many classic Beatles albums, Abbey Road stand above the rest for me thanks to the way they crafted the album into a choesive experience. While the album does hop around different styles, going from the happy go lucky Octopus's Garden to the more rocky I Want You (She's So Heavy), it manages to tie these tracks together. This is especially true in the second half of the album where tracks smoothly transition into each other, bringing in melodies and motifs that come back later. The last three tracks make for an epic finale that reflects on the album as a whole, crescendoing into an overwhelming mix of call backs before coming back down into a beautiful sentiment of shared love."
+            ],
+            [
+                "<strong>2006 | Joanna Newsom</strong>",
+                "<strong>Top Tracks > </strong>Emily, Sawdust and Diamonds",
+                "<strong>My Review > </strong>Over the course of this 5 track, 55 minute album Joanna Newsom delicatly spins immense tales of monkeys, bears, meteors and puppets. There is enough here to write a dissertation or two about but this is not neccassary to enjoy listening to Ys. The imagery alone is so beautiful and tragic in places that no deeper meaning need be understood to bring a tear to your eye. The orchestration is so beautiful and precise, with strings swelling and percussion occasionally rolling in, but the moments when all of that is stripped back to leave Joanna's voice and harp alone are the most compelling."
+            ],
+            [
+                "<strong>2021 | Dijon</strong>",
+                "<strong>Top Tracks > </strong>Many Times, Rodeo Clown",
+                "<strong>My Review > </strong>I have never been reminded so strongly of the passion that comes from performing music than when I listen to Absolutely. This album, along with the short film released of the band performing the best tracks in a strange dining room/garage set, evokes that feeling of being wholely invested in playing the music you love. It seems to almost posses Dijon in his performance on tracks like Many Times, giving you this frantic feeling that the performance has to be at 100%. He goes from yelling out his pain and anger from a past relationship to solemnly wishing for a future that could have been. Every member of the band brings that joy of performing to this album, making it a wonderful colllaborative album to listen to"
+            ]
+        ]
+    }
+}
+console.log("hello");
+for (let f in faves) {
+    changeFave(0,f);
+}
+
+function changeFave(direction,faveType) {
+    faves[faveType].index += direction;
+    faves[faveType].index = faves[faveType].index > 2 ? 2 : faves[faveType].index < 0 ? 0 : faves[faveType].index;
+    document.getElementById(faveType + "-image").setAttribute("src","images/" + faveType + "/" + faves[faveType].images[faves[faveType].index]);
+    document.getElementById(faveType + "-title").innerHTML = faves[faveType].titles[faves[faveType].index];
+    document.getElementById(faveType + "-details").innerHTML = "";
+    for (let d of faves[faveType].details[faves[faveType].index]) {
+        document.getElementById(faveType + "-details").innerHTML += "<p>" + d + "</p>";
+    }
+}
